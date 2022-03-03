f7e3002335b3703900a9a71d509bf6fa3b6a4b1a
chanyadeshani <chanya.deshani@gmail.com>
2022-03-04 04:07:25 +0900

    changed to 23 months of data

--- Assignment.R
+++ Assignment.R
@@ -4,38 +4,41 @@ library(tidyverse)
 
 # Read data from csv to data frame
 df1 = read_csv('principal_offence_category_march_2017.csv', col_names = TRUE)
-march_2017 = read_csv('principal_offence_category_march_2017.csv', col_names = TRUE)
-february_2017 = read_csv('principal_offence_category_february_2017.csv', col_names = TRUE)
-january_2017 = read_csv('principal_offence_category_january_2017.csv', col_names = TRUE)
-december_2016 = read_csv('principal_offence_category_december_2016.csv', col_names = TRUE)
-november_2016 = read_csv('principal_offence_category_november_2016.csv', col_names = TRUE)
-october_2016 = read_csv('principal_offence_category_october_2016.csv', col_names = TRUE)
-september_2016 = read_csv('principal_offence_category_september_2016.csv', col_names = TRUE)
-august_2016 = read_csv('principal_offence_category_august_2016.csv', col_names = TRUE)
-july_2016 = read_csv('principal_offence_category_july_2016.csv', col_names = TRUE)
-june_2016 = read_csv('principal_offence_category_june_2016.csv', col_names = TRUE)
-may_2016 = read_csv('principal_offence_category_may_2016.csv', col_names = TRUE)
-april_2016 = read_csv('principal_offence_category_april_2016.csv', col_names = TRUE)
-january_2016 = read_csv('principal_offence_category_january_2016.csv', col_names = TRUE)
-december_2015 = read_csv('principal_offence_category_december_2015.csv', col_names = TRUE)
-october_2015 = read_csv('principal_offence_category_october_2015.csv', col_names = TRUE)
-september_2015 = read_csv('principal_offence_category_september_2015.csv', col_names = TRUE)
-august_2015 = read_csv('principal_offence_category_august_2015.csv', col_names = TRUE)
-july_2015 = read_csv('principal_offence_category_july_2015.csv', col_names = TRUE)
-june_2015 = read_csv('principal_offence_category_june_2015.csv', col_names = TRUE)
-may_2015 = read_csv('principal_offence_category_may_2015.csv', col_names = TRUE)
+names(df1)
+
+january_2014 = read_csv('principal_offence_category_january_2014.csv', col_names = TRUE)
+february_2014 = read_csv('principal_offence_category_february_2014.csv', col_names = TRUE)
+march_2014 = read_csv('principal_offence_category_march_2014.csv', col_names = TRUE)
+april_2014 = read_csv('principal_offence_category_april_2014.csv', col_names = TRUE)
+may_2014 = read_csv('principal_offence_category_may_2014.csv', col_names = TRUE)
+june_2014 = read_csv('principal_offence_category_june_2014.csv', col_names = TRUE)
+july_2014 = read_csv('principal_offence_category_july_2014.csv', col_names = TRUE)
+august_2014 = read_csv('principal_offence_category_august_2014.csv', col_names = TRUE)
+september_2014 = read_csv('principal_offence_category_september_2014.csv', col_names = TRUE)
+october_2014 = read_csv('principal_offence_category_october_2014.csv', col_names = TRUE)
+november_2014 = read_csv('principal_offence_category_november_2014.csv', col_names = TRUE)
+december_2014 = read_csv('principal_offence_category_december_2014.csv', col_names = TRUE)
+january_2015 = read_csv('principal_offence_category_january_2015.csv', col_names = TRUE)
+february_2015 = read_csv('principal_offence_category_february_2015.csv', col_names = TRUE)
+march_2015 = read_csv('principal_offence_category_march_2015.csv', col_names = TRUE)
 april_2015 = read_csv('principal_offence_category_april_2015.csv', col_names = TRUE)
+may_2015 = read_csv('principal_offence_category_may_2015.csv', col_names = TRUE)
+june_2015 = read_csv('principal_offence_category_june_2015.csv', col_names = TRUE)
+july_2015 = read_csv('principal_offence_category_july_2015.csv', col_names = TRUE)
+august_2015 = read_csv('principal_offence_category_august_2015.csv', col_names = TRUE)
+september_2015 = read_csv('principal_offence_category_september_2015.csv', col_names = TRUE)
+october_2015 = read_csv('principal_offence_category_october_2015.csv', col_names = TRUE)
+december_2015 = read_csv('principal_offence_category_december_2015.csv', col_names = TRUE)
 
 
 # check data type of rows
-class(february_2017$N_AFU)
-head(february_2017)
+head(february_2014)
 
-#Add dataframes to a list
-listOfDataframes = list("Mar_2017" = march_2017, "Feb_2017" = february_2017,"Jan_2017" =january_2017,"Dec_2016"=december_2016,"Nov_2016"=november_2016,"Oct_2016" = october_2016,
-                        "Sep_2016"=september_2016,"Aug_2016"=august_2016,"Jul_2016"=july_2016,"Jun_2016"=june_2016,"May_2016"=may_2016,"Apr_2016"=april_2016,
-                        "Jan_2016"=january_2016,"Dec_2015"=december_2015,"Oct_2015"=october_2015,"Sep_2015"=september_2015,"Aug_2015"=august_2015,"Jul_2015"=july_2015,
-                        "Jun_2015"=june_2015,"May_2015"=may_2015,"Apr_2015"=april_2015)
+#Add data frames to a list
+listOfDataframes = list("Jan_2014" = january_2014, "Feb_2014" = february_2014,"Mar_2014" =march_2014,"Apr_2014"=april_2014,"May_2014"=may_2014,"Jun_2014" = june_2014,
+                        "Jul_2014"=july_2014,"Aug_2014"=august_2014,"Sep_2014"=september_2014,"Oct_2014"=october_2014,"Nov_2014"=november_2014,"Dec_2014"=december_2014,
+                        "Jan_2015"=january_2015,"Feb_2015"=february_2015,"Mar_2015"=march_2015,"Apr_2015"=april_2015,"May_2015"=may_2015,"Jun_2015"=june_2015,
+                        "Jul_2015"=july_2015,"Aug_2015"=august_2015,"Sep_2015"=september_2015,"Oct_2015"=october_2015,"Dec_2015"=september_2015)
 
 colnames = c("Area", "N_HC", "P_HC", "N_HU", "P_HU", "N_OAPC", "P_OAPC", "N_OAPU", "P_OAPU","N_SOC", "P_SOC", "N_SOU", "P_SOU","N_BC",
     "P_BC","N_BU", "P_BU","N_RC","P_RC","N_RU","P_RU","N_THC","P_THC","N_THU","P_THU","N_FFC","P_FFC","N_FFU","P_FFU","N_CDC",
@@ -55,12 +58,13 @@ for (i in seq_along(listOfDataframes)){
   }
 
 }
-
+names(listOfDataframes[[1]])
+view(listOfDataframes[[1]])
 #copy national data to a new data frame
 national <-  data.frame(
   c(listOfDataframes[[i]][1,])
 )
-national[national == "National"] <- "Mar_2017"
+national[national == "National"] <- names(listOfDataframes)[1]
 
 for (i in 2:length(listOfDataframes)){
   national <- rbind(national, listOfDataframes[[i]][1,])
@@ -80,16 +84,15 @@ for (i in seq_along(listOfDataframes)){
 }
 
 #bar graph for N_OAPC from April 2015 ~ March 2017 for National values
+national$Period <- factor(national$Period, levels = national$Period)
+ggplot(national,aes(x=Period,y=N_POOU))+geom_bar(stat="identity")
 
-em <- ggplot(national, aes(Period, N_OAPC))
-em + geom_col()
-
-em <- ggplot(feb_2017, aes(N_OAPC, Area))
-em + geom_col()
+ggplot(listOfDataframes[[4]], aes(N_OAPC ))+geom_boxplot()+ggtitle("Number of Offences Against The Person Convictions in January 2017")+xlab("Number of Offences Against The Person Convictions")
+view(listOfDataframes[1])
 
 #ggplot(feb_2017,aes(x = N_MOU)) + geom_density(fill = "blue")
 
-
+print(listOfDataframes[[2]])
 summary(feb_2017)
 
 feb_2017
