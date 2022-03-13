0b33840a67dbc7f47ac2beea7444b71b1278ce16
chanyadeshani <chanya.deshani@gmail.com>
2022-03-14 01:58:56 +0900

    with mean etc. of time series

--- Assignment.R
+++ Assignment.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(tidyverse)
 library(lubridate)
 library(data.table)
+library(plotly)
 
 # Read data from csv to data frame
 df1 <- read_csv('principal_offence_category_march_2017.csv', col_names = TRUE)
@@ -33,7 +34,6 @@ december_2015 <- read_csv('principal_offence_category_december_2015.csv', col_na
 
 colnames(df1)
 # check data type of rows
-head(february_2014)
 
 #Add data frames to a list name of the data frame is set so that it can change to a date later
 listOfDataframes <- list("1Jan2014" = january_2014, "1Feb2014" = february_2014,"1Mar2014" =march_2014,"1Apr2014"=april_2014,"1May2014"=may_2014,"1Jun2014" = june_2014,
@@ -41,7 +41,7 @@ listOfDataframes <- list("1Jan2014" = january_2014, "1Feb2014" = february_2014,"
                         "1Jan2015"=january_2015,"1Feb2015"=february_2015,"1Mar2015"=march_2015,"1Apr2015"=april_2015,"1May2015"=may_2015,"1Jun2015"=june_2015,
                         "1Jul2015"=july_2015,"1Aug2015"=august_2015,"1Sep2015"=september_2015,"1Oct2015"=october_2015,"1Dec2015"=december_2015)
 
-colnames <- c("Police", "N_HC", "P_HC", "N_HU", "P_HU", "N_OAPC", "P_OAPC", "N_OAPU", "P_OAPU","N_SOC", "P_SOC", "N_SOU", "P_SOU","N_BC",
+colnames <- c("court ", "N_HC", "P_HC", "N_HU", "P_HU", "N_OAPC", "P_OAPC", "N_OAPU", "P_OAPU","N_SOC", "P_SOC", "N_SOU", "P_SOU","N_BC",
     "P_BC","N_BU", "P_BU","N_RC","P_RC","N_RU","P_RU","N_THC","P_THC","N_THU","P_THU","N_FFC","P_FFC","N_FFU","P_FFU","N_CDC",
     "P_CDC","N_CDU","P_CDU","N_DOC","P_DOC","N_DOU","P_DOU","N_POOC","P_POOC","N_POOU","P_POOU","N_OTHERC","P_OTHERC","N_OTHERU",
     "P_OTHERU","N_MOC", "P_MOC","N_MOU","P_MOU","N_AFU","P_LMOU" )
@@ -54,10 +54,8 @@ for (i in seq_along(listOfDataframes)){
   
   # Correct the first column data that was set to NA when converting data to NA
   for (j in 1:nrow(listOfDataframes[[i]])){
-    
     listOfDataframes[[i]][j,1]<-df1[j,1]
   }
-
 }
 
 #copy national data to a new data frame
@@ -69,7 +67,7 @@ for (i in 2:length(listOfDataframes)){
   national[national == "National"] <- names(listOfDataframes)[i]
 }
 
-view(national)
+#view(national)
 # Change the first column name without changing others
 colnames(national) <- c("Period", "N_HC", "P_HC", "N_HU", "P_HU", "N_OAPC", "P_OAPC", "N_OAPU", "P_OAPU","N_SOC", "P_SOC", "N_SOU", "P_SOU","N_BC",
                           "P_BC","N_BU", "P_BU","N_RC","P_RC","N_RU","P_RU","N_THC","P_THC","N_THU","P_THU","N_FFC","P_FFC","N_FFU","P_FFU","N_CDC",
@@ -77,14 +75,15 @@ colnames(national) <- c("Period", "N_HC", "P_HC", "N_HU", "P_HU", "N_OAPC", "P_O
                           "P_OTHERU","N_MOC", "P_MOC","N_MOU","P_MOU","N_AFU","P_LMOU" ) 
 
 
+
 # Drop the row with National data
 for (i in seq_along(listOfDataframes)){
   
-  listOfDataframes[[i]] <- listOfDataframes[[i]][!(listOfDataframes[[i]]$Area %in% c("National")), ]
+  listOfDataframes[[i]] <- listOfDataframes[[i]][!(listOfDataframes[[i]]$court  %in% c("National")), ]
 }
 
 # Find total number of offences 
-df_Total <- 
+#df_Total <- 
 
 #bar graph for N_OAPC from April 2015 ~ March 2017 for National values
 national$Period <- factor(national$Period, levels = national$Period)
@@ -102,11 +101,11 @@ view(listOfDataframes[[1]])
 
 print(listOfDataframes[[2]])
 summary(feb_2017)
-df_N_OAPC <- listOfDataframes[[1]][ , ("Police")]
-for (i in 1:seq_along(listOfDataframes)){
+df_N_OAPC <- listOfDataframes[[1]][ , ("court ")]
+for (i in seq_along(listOfDataframes)){
   df_N_OAPC <- cbind(df_N_OAPC, listOfDataframes[[i]][,6])
   }
-colnames(df_N_OAPC) <- c("Police", "Jan_2014", "Feb_2014","Mar_2014","Apr_2014","May_2014","Jun_2014","Jul_2014","Aug_2014"
+colnames(df_N_OAPC) <- c("court ", "Jan_2014", "Feb_2014","Mar_2014","Apr_2014","May_2014","Jun_2014","Jul_2014","Aug_2014"
                          ,"Sep_2014","Oct_2014","Nov_2014","Dec_2014","Jan_2015","Feb_2015","Mar_2015","Apr_2015","May_2015",
                          "Jun_2015", "Jul_2015","Aug_2015","Sep_2015","Oct_2015","Dec_2015" ) 
 view(df_N_OAPC)
@@ -144,13 +143,10 @@ ggplot(data=dft_N_OAPC, aes(x=Period, y=Avon_and_Somerset)) +
   geom_bar()
 ggplot(dft_N_OAPC, aes(x=Period, y= Avon_and_Somerset)) + geom_bar(stat='identity')
 
-data %>%
-  ggplot( aes(x=date, y=value)) +
-  geom_line(color="#69b3a2")
 listOfDataframes[[4]]%>%ggplot(aes(y = Area, x=P_OAPC , group = 1)) + 
   geom_bar(color="#69b3a2",stat='identity')+ 
-  xlab("Percentage of Offences Against The Person Convictions") + ylab("Police Force") +
-  ggtitle("Percentage of offences against the person convictions by the police force")
+  xlab("Percentage of Offences Against The Person Convictions") + ylab("Court ") +
+  ggtitle("Percentage of offences against the person convictions by the Court for January 2014")
 
 # adding date as the first of the month that data published to convert data in to time series and merge
 for (i in seq_along(listOfDataframes)){
@@ -160,5 +156,64 @@ timeseriesdf <- listOfDataframes[[1]]
 for (i in 2:length(listOfDataframes)){
   timeseriesdf <- rbind(timeseriesdf,listOfDataframes[[i]])
 }
-nrow(listOfDataframes[[1]])
-listOfDataframes[[1]]
+timeseriesdf$court  <- ifelse(timeseriesdf$court  %in% c('Metropolitan and City'), "Metropolitan & City",timeseriesdf$court)
+timeseriesdf$court  <- ifelse(timeseriesdf$court %in% c('Avon and Somerset'), "Avon & Somerset",timeseriesdf$court)
+
+
+# x scale copied from https://ggplot2-book.org/scale-position.html#date-labels
+p <- timeseriesdf %>%
+  ggplot( aes(x=Date, y=P_HC, group=court, fill=court)) +
+  geom_area(stat='identity') +
+  ggtitle("Percentage of Homicide Convictions from 2014 to 2015") +
+  scale_x_date(limits = as.Date(c("2014-01-01", "2015-12-01")), 
+               labels = scales::label_date_short())+ 
+  theme(
+    legend.position="center",
+  ) + facet_wrap(~court, scale="free_y") 
+
+                                                         
+ggsave(p, filename = "Percentage of Homicide Convictions.png")
+names(timeseriesdf)
+unique(timeseriesdf$Police)
+names(df1)
+mean_vec <- vector()
+median_vec <- vector()
+sd_vec <- vector()
+x <- vector()
+for (i in 2:(ncol(timeseriesdf) -1)){
+  x[i] <- colnames(timeseriesdf)[i]
+  mean_vec[i] <- mean(timeseriesdf[ ,i],na.rm=TRUE)
+  median_vec[i] <- median(timeseriesdf[ ,i],na.rm=TRUE)
+  sd_vec[i] <- sd(timeseriesdf[ ,i],na.rm=TRUE)
+}
+stats_pc <- data.frame(Principal_crime = x, 
+                       mean_values = mean_vec, 
+                       median_values = median_vec, 
+                       sd_values = sd_vec)
+stats_pc <- stats_pc[ -1, ] 
+
+colnames(stats_pc)
+
+p1 <- ggplot(stats_pc, aes(Principal_crime, mean_values, group = 1)) + 
+  geom_line()+
+  ggtitle("Mean value of the number of court cases finalised by principal offence") +
+  theme(axis.text.x=element_text(angle=90,vjust =1))
+
+ggplotly(p1)
+
+p2 <- ggplot(stats_pc, aes(Principal_crime, median_values, group = 1)) + 
+  geom_line()+
+  ggtitle("Mean value of the number of court cases finalised by principal offence") +
+  theme(axis.text.x=element_text(angle=90,vjust =1))
+
+ggplotly(p2)
+p3 <- ggplot(stats_pc, aes(Principal_crime, sd_values, group = 1)) + 
+  geom_line()+
+  ggtitle("Standard deviation value of the number of court cases finalised by principal offence") +
+  theme(axis.text.x=element_text(angle=90,vjust =1))
+
+ggplotly(p3)
+
+dfbfs <-filter(timeseriesdf, Police == "Bedfordshire")
+summary(dfbfs)
+
